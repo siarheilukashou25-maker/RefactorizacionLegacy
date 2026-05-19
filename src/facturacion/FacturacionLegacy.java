d131f6ec95c30d4862666c6814eb98da3921de45
Siarhei <siarheilukashou.25@campuscamara.es>
2026-05-19 14:36:00 +0200

    Carmen. He documentado el código con la documentacion JavaDoc y he refactorizado el código.

--- src/facturacion/FacturacionLegacy.java
+++ src/facturacion/FacturacionLegacy.java
@@ -1,5 +1,6 @@
 package facturacion;
 /**
+ * @author Carmen Alhaja García y Siarhei Lukashou
  * Clase Legacy del sistema de facturación.
  * ADVERTENCIA: Código con alta deuda técnica. No modificar la firma del método.
  */
@@ -8,25 +9,35 @@ public class FacturacionLegacy {
     public static final double DESCUENTO_ESTANDAR = 0.85;
     public static final double DESCUENTO_TERCER_TIPO = 0.95;
     
-    // Método a refactorizar
+    /**
+     * Devuelve importe total según tipo de cliente.
+     * @param importeBase Importe base del que calculamos.
+     * @param tipoCliente Tipo de cliente 
+     * @param esSocioVip Si es socio VIP. Si es true tendrá un descuento.
+     * @return Devuelve variable con el valor de importe total de tipo double.
+     * Con rango de valores [0, Double.MAX_VALUE]
+     * @see java.lang.Double
+     */
     public double calcularTotal(double importeBase, int tipoCliente, boolean esSocioVip) {
         //Entorno:
             double importeTotal;
         //Algoritmo:
             importeTotal = 0;
             
-            switch(tipoCliente){
-                case 1:
-                    importeTotal = esSocioVip ? importeBase * DESCUENTO_VIP : importeBase * DESCUENTO_ESTANDAR;
-                    break;
-                case 2:
-                    importeTotal = importeBase * DESCUENTO_TERCER_TIPO;
-                    break;
-                default:
-                    importeTotal = importeBase;
-                    break;
-            }
-                
+            if(importeTotal > 0){
+                switch(tipoCliente){
+                    case 1:
+                        importeTotal = esSocioVip ? importeBase * DESCUENTO_VIP : importeBase * DESCUENTO_ESTANDAR;
+                        break;
+                    case 2:
+                        importeTotal = importeBase * DESCUENTO_TERCER_TIPO;
+                        break;
+                    default:
+                        importeTotal = importeBase;
+                        break;
+                }//Fin Segun Sea
+            }//Fin Si
+   
             return importeTotal;
     }
 }
